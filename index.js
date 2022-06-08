d9f847cfdb5cf39e3724c9c497277d9aba693b91
Sergio León <sergioleon4004@hotmail.com>
2022-06-08 12:00:12 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+(function() {
+    const list = document.querySelectorAll('ul#list-computer > li.list-group-item');
+    const products = [];
+
+    for (let child of list) {
+        if (typeof child === 'function' || typeof child === 'number') continue;
+
+        products.push({
+            title: child.querySelector('h4').textContent.trim(),
+            description: child.querySelector('p').textContent.trim()
+        });
+    }
+
+    return { jobs: products };
+})();
